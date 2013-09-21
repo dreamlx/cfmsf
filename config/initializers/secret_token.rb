6186be5db9fb6ac1d611c0e03dfbb707dccce570
HuberyDu <duxiaolong92@gmail.com>
2013-09-21 13:26:37 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bancheng::Application.config.secret_token = '62b8e2859a9e59d3f6b73d43d671c81af401f6e902fbc4289f5d5ee7b8ae0e7fff494dca177529d9f54d27a9b7e6b8a3b565887e2cd61a4985cdb76d95dfdd89'
