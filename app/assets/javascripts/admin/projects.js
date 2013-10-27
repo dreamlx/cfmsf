b6438fbae521bff899eb5e90d395adff0d682d69
HuberyDu <duxiaolong92@gmail.com>
2013-10-27 15:26:18 +0800

    修正work和project截图的错误

--- app/assets/javascripts/admin/projects.js
+++ app/assets/javascripts/admin/projects.js
@@ -1,4 +1,49 @@
 $(document).ready(function(){
 	$('input.datepicker').datepicker({format: 'yyyy-mm-dd'});
-})
+	$("#image_file").change(function(){
+        readURL(this);
+    });
 
+    var jcrop_api;
+
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                // if()
+                $('#preview').attr('src', e.target.result);
+                $('#preview').Jcrop({
+                    onChange:   updateInfo,
+                    onSelect:   updateInfo,
+                    onRelease:  clearInfo
+                },function(){
+                    jcrop_api = this;
+                });
+            }
+            
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    
+	function updateInfo(c)
+    {
+        $('#x1').val(c.x);
+        $('#y1').val(c.y);
+        $('#x2').val(c.x2);
+        $('#y2').val(c.y2);
+        $('#width').attr('type', 'text')
+        $('#heigth').attr('type', 'text')
+        $('#width').val(c.x2 - c.x);
+        $('#heigth').val(c.y2 - c.y);
+    };
+
+    function clearInfo()
+    {
+        $('#x1').val("");
+        $('#y1').val("");
+        $('#x2').val("");
+        $('#y2').val("");
+        $('#width').val("");
+        $('heigth').val("")
+    };
+})
--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -3,5 +3,18 @@ module Admin
   class BaseController < ApplicationController
     before_filter :authenticate_user!
     layout 'admin'
+
+    def crop_image(thumb)
+      avatar_path = Rails.root + "public/uploads/image"
+      photo_name = "thumb.jpg"
+      img = MiniMagick::Image.from_blob(thumb.read)
+      Dir.chdir avatar_path
+      img.write "#{photo_name}"
+      img = MiniMagick::Image.open("#{photo_name}")  
+      width = params[:x2].to_i - params[:x1].to_i  
+      height= params[:y2].to_i - params[:y1].to_i 
+      img.crop "#{width}x#{height}+#{params[:x1]}+#{params[:y1]}"  
+      thumb = img
+    end
   end
 end
