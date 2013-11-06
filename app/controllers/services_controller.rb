3619afcacb771d2914ffd6fcd8d3565a26c49836
Hotspring <hotspring0219@gmail.com>
2013-11-07 01:42:54 +0800

    调整首页project排序顺序 调整services页面，获取所有client信息

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -2,6 +2,12 @@ class ServicesController < ApplicationController
 	def index
 		@css = "site_services"
 	    recommendProjects
-	    @clients = Client.all
+	    # @clients = Client.all
+	    @services_1 = Client.where("service = 1")
+	    @services_2 = Client.where("service = 2")
+	    @services_3 = Client.where("service = 3")
+	    @services_4 = Client.where("service = 4")
+	    @services_5 = Client.where("service = 5")
+	    @services_6 = Client.where("service = 6")
 	end
 end
