6947bbec3cb30f4932c525ee67a93be3f8a59039
小龙 <duxiaolong92@gmail.com>
2014-01-03 18:57:03 +0800

    modifed js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,8 @@ Bancheng::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( 
-    site.css
+    site.css,
+    admin/users.js
    )
 
   # Disable delivery errors, bad email addresses will be ignored
