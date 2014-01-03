117900b0e57e0c2e9ba9871086055717c9fc9987
小龙 <duxiaolong92@gmail.com>
2014-01-03 16:46:32 +0800

    删除thinking文件

--- app/indices/life_index.rb
+++
@@ -1,5 +0,0 @@
-ThinkingSphinx::Index.define :life, :with => :active_record do
-  indexes title, content
-
-  has published
-end
--- app/indices/project_index.rb
+++
@@ -1,4 +0,0 @@
-ThinkingSphinx::Index.define :project, :with => :active_record do
-  indexes tag_list
-  has published
-end
