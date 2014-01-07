78de73dd81191a3ef263337ad563c2fe8551a20a
小龙 <duxiaolong92@gmail.com>
2014-01-07 15:11:45 +0800

    remove index page authenticate_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   # reset captcha code after each request for security
-  before_filter :authenticate_user!
   layout "application"
 
   protect_from_forgery
