251b03c5e233a3d58ef321fb05cec18c8e202739
HuberyDu <duxiaolong92@gmail.com>
2014-01-22 19:52:56 +0800

    add authenticate user

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,7 +1,7 @@
 module Admin
   
   class BaseController < ApplicationController
-    # before_filter :authenticate_user!
+    before_filter :authenticate_user!
     layout 'admin'
     rescue_from CanCan::AccessDenied do |exception|
       redirect_to admin_home_index_path, :alert => exception.message
